fd5156650588115c63a0861b43a885794ffa76c7
Shaharyar <Shaharyarsajid@gmail.com>
2021-06-01 16:09:10 +0500

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,64 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:shake/shake.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(BallPage());
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue.shade300,
+        appBar: AppBar(
+          backgroundColor: Colors.blue.shade700,
+          title: Center(
+            child: Text(
+              'Ask me anything',
+            ),
+          ),
+        ),
+        body: Ball(),
       ),
     );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  void initState() {
+    super.initState();
+    ShakeDetector detector = ShakeDetector.autoStart(
+      onPhoneShake: () {
+        setState(() {
+          ballNumber = Random().nextInt(5) + 1;
+        });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: FlatButton(
+          child: Image.asset('images/ball$ballNumber.png'),
+          onPressed: () {
+            setState(() {
+              ballNumber = Random().nextInt(5) + 1;
+            });
+            print('Button Pressed\n number $ballNumber generated');
+          },
+        ),
+      ),
+    );
+  }
+}
